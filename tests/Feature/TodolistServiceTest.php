3263f10d426dd526cb731b7155fe83d4e4dfd344
budi-afk <budiman1347@gmail.com>
2024-04-14 16:08:38 +0700

    create logic saveTodo

--- tests/Feature/TodolistServiceTest.php
+++ tests/Feature/TodolistServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Services\TodolistService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class TodolistServiceTest extends TestCase
@@ -20,5 +21,19 @@ class TodolistServiceTest extends TestCase
     public function testTodolistNotNull()
     {
         $this->assertNotNull($this->todolistService);
+    }
+
+    public function testSaveTodo()
+    {
+        $this->todolistService->saveTodo("1", "test");
+
+        $todolist = Session::get('todolist');
+        self::assertEquals("1", $todolist[0]['id']);
+        self::assertEquals("test", $todolist[0]['todo']);
+
+        // foreach($todolist as $value) {
+        //     self::assertEquals("1", $value['id']);
+        //     self::assertEquals("test", $value['todo']);
+        // }
     }
 }
